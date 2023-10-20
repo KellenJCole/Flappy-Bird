736011f8d1271e17e55d2fde2e68035976905f5c
Kellen Cole <kellencole03@gmail.com>
2023-10-19 19:17:07 -0700

    Setup ground sprites

--- Flappy Bird/Base.cpp
+++ Flappy Bird/Base.cpp
@@ -9,6 +9,12 @@ Base::Base(sf::RenderWindow* w) {
 
 	baseTex.loadFromFile("Resources/base.png");
 	baseSprite.setTexture(baseTex);
+
+	baseSprites[0] = new sf::Sprite(baseSprite);
+	baseSprites[1] = new sf::Sprite(baseSprite);
+
+	baseSprites[0]->setPosition(0, 400);
+	baseSprites[1]->setPosition(336, 400);
 }
 
 void Base::update(sf::Time delta) {
@@ -16,5 +22,6 @@ void Base::update(sf::Time delta) {
 }
 
 void Base::draw() {
-
+	gameWindow->draw(*baseSprites[0]);
+	gameWindow->draw(*baseSprites[1]);
 }
