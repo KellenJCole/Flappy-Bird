96ba9325f4d9e0e021a2f791e8eeb4edd8fde4b3
Kellen Cole <kellencole03@gmail.com>
2023-10-19 19:26:13 -0700

    Finish ground logic

--- Flappy Bird/Base.cpp
+++ Flappy Bird/Base.cpp
@@ -19,6 +19,18 @@ Base::Base(sf::RenderWindow* w) {
 
 void Base::update(sf::Time delta) {
 
+	// First check if a sprite is completely out of view
+	if (baseSprites[0]->getPosition().x < -336) {
+		// If so, move the sprite behind the one following it and adjust index accordingly
+		baseSprites[0]->setPosition(baseSprites[1]->getPosition().x + 336, 400);
+		std::swap(baseSprites[0], baseSprites[1]);
+	}
+	float scrollSpeed = -100.f; // Adjust the rate at which the ground appears to pass the bird
+	float distanceToScroll = scrollSpeed * delta.asSeconds(); // Use linear interpolation to calculate distance to move ground based off time passed
+
+	// Adjust ground position accordingly
+	baseSprites[0]->setPosition(baseSprites[0]->getPosition().x + distanceToScroll, 400);
+	baseSprites[1]->setPosition(baseSprites[1]->getPosition().x + distanceToScroll, 400);
 }
 
 void Base::draw() {
--- Flappy Bird/Base.h
+++ Flappy Bird/Base.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 
 class Base {
 public:
