e6c4baff95912b8ae53de52e03224b9ab22d3096
Kellen Cole <kellencole03@gmail.com>
2023-10-20 12:50:05 -0700

    Create Pipe class header

---
+++ Flappy Bird/Pipes.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Pipes {
+	Pipes();
+	Pipes(sf::RenderWindow* w);
+	void updatePipes(sf::Time dt);
+	void draw();
+private:
+	sf::RenderWindow* w;
+	sf::Texture pipeUpTex, pipeDownTex;
+	sf::Sprite pipeUpSprite, pipeDownSprite;
+	sf::Time pipeSpawnTimer;
+	sf::Sprite* pipes[8];
+};
