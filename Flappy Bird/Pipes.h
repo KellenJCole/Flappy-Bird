c0bdb8f2061d88213bfce261c9f159e58d26c939
Kellen Cole <kellencole03@gmail.com>
2023-10-20 13:36:31 -0700

    Add collision variables for pipes

--- Flappy Bird/Pipes.h
+++ Flappy Bird/Pipes.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 class Pipes {
 public:
@@ -7,10 +8,12 @@ public:
 	Pipes(sf::RenderWindow* w);
 	void updatePipes(sf::Time dt);
 	void draw();
+	std::vector<sf::RectangleShape> getCollision();
 private:
 	sf::RenderWindow* gameWindow;
 	sf::Texture pipeUpTex, pipeDownTex;
 	sf::Sprite pipeUpSprite, pipeDownSprite;
 	sf::Time pipeSpawnTimer;
 	sf::Sprite* pipes[8];
+	std::vector<sf::RectangleShape> collisionBoxes;
 };
