38eb4e3ae308a2791ecb9d29e4bf8ddd66636339
Kellen Cole <kellencole03@gmail.com>
2023-10-19 18:53:26 -0700

    Create empty function definitions

---
+++ Flappy Bird/GameLoop.cpp
@@ -0,0 +1,21 @@
+#include "GameLoop.h"
+
+GameLoop::GameLoop() {
+
+}
+
+void GameLoop::run() {
+
+}
+
+void GameLoop::processEvents() {
+
+}
+
+void GameLoop::update(sf::Time delta) {
+
+}
+
+void GameLoop::render() {
+
+}
