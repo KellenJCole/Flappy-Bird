3256b5f0b23d61958b735247d0cc9d60ccdb3bea
Kellen Cole <kellencole03@gmail.com>
2023-10-19 18:56:08 -0700

    Setup window and game loop

--- Flappy Bird/GameLoop.cpp
+++ Flappy Bird/GameLoop.cpp
@@ -1,11 +1,20 @@
 #include "GameLoop.h"
 
 GameLoop::GameLoop() {
+	window.create(sf::VideoMode(288, 512), "Flappy Bird");
 
+	window.display();
 }
 
 void GameLoop::run() {
-
+	sf::Clock gameClock;
+	sf::Time dt;
+	while (window.isOpen()) {
+		dt = gameClock.restart();
+		processEvents();
+		update(dt);
+		render();
+	}
 }
 
 void GameLoop::processEvents() {
