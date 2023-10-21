09b229489683d84e34ee593ed15576a58d15861f
Kellen Cole <kellencole03@gmail.com>
2023-10-20 17:51:21 -0700

    Score display now works for all scores

--- Flappy Bird/Score.cpp
+++ Flappy Bird/Score.cpp
@@ -13,28 +13,41 @@ Score::Score(sf::RenderWindow* w) {
 }
 
 void Score::setScore(int s) {
+	for (auto s : scoreSprites) {
+		delete s;
+	}
 
-	if (s <= 9) {
-		scoreSprites.resize(1);
-		scoreSprites[0] = new sf::Sprite;
-		sf::Sprite onesSprite;
-		onesSprite.setTexture(digitTex[s]);
-		sf::FloatRect onesBounds = onesSprite.getLocalBounds();
-		onesSprite.setOrigin(onesBounds.left + onesBounds.width / 2, onesBounds.top + onesBounds.height / 2);
-		*scoreSprites[0] = onesSprite;
+	scoreSprites.clear();
+	if (s != 0) {
+		scoreSprites.resize(log10(s) + 1);
+
+		int places = (int)log10(s) + 1;
+
+		int sCopy = s;
+
+		int placeInQuestion = places - 1;
+		for (int i = 1; i <= places; i++) {
+			int digit = sCopy % 10;
+			sCopy /= 10;
+
+			scoreSprites[placeInQuestion] = new sf::Sprite;
+			sf::Sprite newSprite;
+			newSprite.setTexture(digitTex[digit]);
+			sf::FloatRect newBounds = newSprite.getLocalBounds();
+			newSprite.setOrigin(newBounds.left + newBounds.width / 2, newBounds.top + newBounds.height / 2);
+			*scoreSprites[placeInQuestion] = newSprite;
+			placeInQuestion--;
+		}
 	}
 	else {
-		bool makeNewDigitsPlace = true;
-		int base = 10;
-		while (makeNewDigitsPlace) {
-			if (s >= base) {
+		scoreSprites.resize(1);
 
-			}
-			else {
-				makeNewDigitsPlace = false;
-			}
-			base *= 10;
-		}
+		scoreSprites[0] = new sf::Sprite;
+		sf::Sprite zeroSprite;
+		zeroSprite.setTexture(digitTex[0]);
+		sf::FloatRect zeroBounds = zeroSprite.getLocalBounds();
+		zeroSprite.setOrigin(zeroBounds.left + zeroBounds.width / 2, zeroBounds.top + zeroBounds.height / 2);
+		*scoreSprites[0] = zeroSprite;
 	}
 }
 
@@ -42,7 +55,7 @@ void Score::draw() {
 	int startX = 153 - (scoreSprites.size() * scoreSprites[0]->getLocalBounds().width / 2);
 
 	for (int i = 0; i < scoreSprites.size(); i++) {
-		scoreSprites[i]->setPosition(sf::Vector2f(startX, 100));
+		scoreSprites[i]->setPosition(sf::Vector2f(startX + (i * 30), 100));
 		gameWindow->draw(*scoreSprites[i]);
 	}
 }
